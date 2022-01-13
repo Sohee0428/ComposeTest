ed32593bb729fe915ed31141037d81840b955423
권소희 <sohee3480@gmail.com>
2022-01-13 16:31:35 +0900

    compose Activity 만들기

---
+++ app/src/main/java/com/sohee/compose/ComposeActivity.kt
@@ -0,0 +1,13 @@
+package com.sohee.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class ComposeActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+        }
+    }
+}
